d097d80ee3e6699ddea32ad5a7cbf40748eb4937
W3bbo_cp <SND\W3bbo_cp>
2010-12-27 18:59:50 +0000

    Consolidating projects together.

---
+++ Anolis.Tools.ImageTools/AlphaBitmap/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Anolis.Tools.AlphaBitmap {
+	
+	public partial class MainForm : Form {
+		
+		public MainForm() {
+			InitializeComponent();
+			
+			this.__alphaBrowse.Click += new EventHandler(__alphaBrowse_Click);
+			this.__colorBrowse.Click += new EventHandler(__colorBrowse_Click);
+			
+			this.__compose.Click += new EventHandler(__compose_Click);
+			
+			this.__save.Click += new EventHandler(__save_Click);
+		}
+		
+		private void __save_Click(object sender, EventArgs e) {
+			
+			Image final = __final.Image;
+			if( final == null ) return;
+			
+			if( __sfd.ShowDialog(this) == DialogResult.OK ) {
+				
+				if( __sfd.FilterIndex == 1 ) {
+					
+					final.Save( __sfd.FileName, ImageFormat.Bmp );
+					
+				} else if( __sfd.FilterIndex == 2 ) {
+					
+					final.Save( __sfd.FileName, ImageFormat.Png );
+					
+				}
+				
+			}
+			
+		}
+		
+		private void __colorBrowse_Click(object sender, EventArgs e) {
+			
+			if( __ofd.ShowDialog(this) == DialogResult.OK ) {
+				
+				__colorSource.Text = __ofd.FileName;
+				
+				Image img = Image.FromFile( __ofd.FileName );
+				__color.Image = img;
+				
+			}
+			
+		}
+		
+		private void __alphaBrowse_Click(object sender, EventArgs e) {
+			
+			if( __ofd.ShowDialog(this) == DialogResult.OK ) {
+				
+				__alphaSource.Text = __ofd.FileName;
+				
+				Image img = Image.FromFile( __ofd.FileName );
+				__alpha.Image = img;
+				
+			}
+			
+		}
+		
+		private void __compose_Click(object sender, EventArgs e) {
+			
+			Bitmap color = __color.Image as Bitmap;
+			Bitmap alpha = __alpha.Image as Bitmap;
+			
+			Bitmap final = new Bitmap( color.Width, color.Height, PixelFormat.Format32bppArgb );
+			
+			// the inefficient way for purity right now
+			
+			for(int x=0;x<color.Width;x++ ) {
+				
+				for(int y=0;y<color.Height;y++) {
+					
+					Color c = color.GetPixel( x, y );
+					Color a = alpha.GetPixel( x, y );
+					
+					Int32 ac;
+					switch(alpha.PixelFormat) {
+						case PixelFormat.Format32bppArgb:
+							
+							ac = a.A;
+							break;
+							
+						case PixelFormat.Format24bppRgb:
+						default:
+							
+							ac = a.R;
+							break;
+					}
+					
+					Color f = Color.FromArgb( ac, c.R, c.G, c.B );
+					
+					final.SetPixel( x, y, f );
+					
+				}
+			}
+			
+			__final.Image = final;
+			
+		}
+		
+	}
+}
---
+++ Anolis.Tools.TweakUI/FileTypes/TypeEditor.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+using Anolis.Core.Utility;
+
+namespace Anolis.FileTypes {
+	
+	public partial class TypeEditor : UserControl {
+		
+		public TypeEditor() {
+			InitializeComponent();
+		}
+		
+		public void ShowFileType(FileType type) {
+			
+			this.__progId      .Text = type.ProgId;
+			this.__friendlyName.Text = type.FriendlyName;
+			this.__iconPath    .Text = type.DefaultIcon;
+			
+			Image old = this.__icon.Image;
+			if( old != null ) old.Dispose();
+			
+			this.__icon.Image = GetIconImage( type.DefaultIcon );
+			
+			__verbs.Items.Clear();
+			foreach(String verb in type.ShellVerbs.Keys) {
+				
+				String line = verb + @" \ " + type.ShellVerbs[verb];
+				
+				__verbs.Items.Add( line );
+			}
+			
+		}
+		
+		private static Bitmap GetIconImage(String defaultIconExpr) {
+			
+			if( String.IsNullOrEmpty( defaultIconExpr ) ) return null;
+			
+			String path = defaultIconExpr;
+			
+			String idxStr = "";
+			
+			Int32 commaIdx = defaultIconExpr.LastIndexOf(',');
+			if( commaIdx > 0 ) {
+				idxStr = path.Substring( commaIdx + 1 );
+				path   = path.Substring( 0, commaIdx );
+			}
+			
+			Icon ico;
+			
+			if( idxStr.Length > 0 ) {
+				
+				Int32 idx = -1;
+				if( !Int32.TryParse( idxStr, NumberStyles.Integer, CultureInfo.InvariantCulture, out idx ) ) {
+					return null;
+				}
+				
+				ico = NativeMethods.ExtractIcon( path, idx );
+				
+				if( ico == null ) {
+					return null;
+				}
+				
+			} else if( IsValidIconPath( path ) ) {
+				
+				try {
+					
+					ico = new Icon( path );
+					
+				} catch(IOException) {
+					return null;
+				}
+				
+			} else {
+				
+				return null;
+			}
+			
+			if( ico.Width == 0 || ico.Height == 0 ) {
+				DestroyIcon( ico );
+				return null;
+			}
+			
+			Bitmap ret = ico.ToBitmap();
+			
+			DestroyIcon( ico );
+			
+			return ret;
+			
+		}
+		
+		private static void DestroyIcon(Icon icon) {
+			
+			// this is needed because Icon.Dispose doesn't Destroy its handle if it was made .FromHandle
+			
+			IntPtr handle = icon.Handle;
+			icon.Dispose();
+			NativeMethods.DestroyIcon( handle );
+			
+		}
+		
+		private static Boolean IsValidIconPath(String path) {
+			
+			Char[] invalid = Path.GetInvalidPathChars();
+			if( path.IndexOfAny( invalid ) >= 0 ) {
+				return false;
+			}
+			
+			return Path.IsPathRooted( path ) && File.Exists( path );
+		}
+		
+	}
+}
